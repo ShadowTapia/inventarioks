bb949457d129dc5414a34a7c3b50dcc718b77d6f
Marcelo Tapia <chelo.tapia@gmail.com>
2024-05-06 16:07:20 -0400

    Se modifica la vista de impresión de códigos de barra para todos los dispositivos

--- app/Http/Controllers/PrintCodebarAll.php
+++ app/Http/Controllers/PrintCodebarAll.php
@@ -3,25 +3,44 @@
 namespace App\Http\Controllers;
 
 use App\Models\devices;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
+use Picqer\Barcode\BarcodeGeneratorPNG;
 
 class PrintCodebarAll extends Controller
 {
     public function index()
     {
-        $devices = array();
-        $device = devices::all();
-        if ($device > 0) {
-            foreach ($device as $dispo) {
-                $devices[] = $dispo;
-            }
-        }
+        $devlist = devices::all();
 
-        return view('livewire.components.printCodebarAll', ['devices' => $devices]);
+        return view('livewire.components.printCodebarAll', compact('devlist'));
     }
+
+    /**
+     * Se encarga de imprimir para todos los dispositivos sus códigos de barra
+     */
 
     public function printCodeAll()
     {
-        $device = null;
+        $devices = array();
+
+        $devlist = devices::all();
+
+
+        if ($devlist->count() > 0) {
+            $conta = 0;
+            foreach ($devlist as $devi) {
+                $conta++;
+
+                $generator = new BarcodeGeneratorPNG();
+                $barcode = base64_encode($generator->getBarcode($devi->numserie, $generator::TYPE_CODE_128));
+                $devices[$conta][0] = $devi->product->name;
+                $devices[$conta][1] = $devi->product->modelo;
+                $devices[$conta][2] = $barcode;
+            }
+        }
+
+        $pdf = App::make('dompdf.wrapper');
+        $pdf->loadView('livewire.components.printCodebarAll', compact('devices'));
+        return $pdf->stream();
     }
 }
--- resources/views/livewire/components/printCodebarAll.blade.php
+++ resources/views/livewire/components/printCodebarAll.blade.php
@@ -1,28 +1,55 @@
 <x-fondo-print>
     <x-slot name="content">
-        @if ($devices->count())
+
             <table>
+                <caption class="caption-top">
+                    <h1>Listado de Dispositivos</h1>
+                </caption>
+
                 <tbody>
-                    <table>
+                    @php
                         $conta=0;
-                        @foreach ($devices as $devi)
-                            $conta++;
-                            @if (($conta % 5)==0)
+                    @endphp
+                    @if ($devices)
+                        @foreach ($devices as $key=>$value)
+                                @php
+                                    $conta++;
+                                @endphp
+                                @php
+                                    if (($conta % 3)==0) {
+                                @endphp
                                 <tr>
-                            @endif
-                            <td class="pl-3 pr-3">
-                                <div>$conta</div>
-                                <div>{{ $devi->producto }}</div>
-                                <div>{{ $devi->modelo }}</div>
-                            </td>
+                                @php
+                                    }
+                                @endphp
+
+                                    <td style="padding-right: 15px;padding-left: 15px;">
+                                        <div class="grid grid-cols-1 gap-4 max-h-fit">
+                                            <div class="left-8">
+                                                <img id="logoks" src="{{ asset('images/kscodebar.png') }}" alt="">
+                                            </div>
+                                            <div>{{ $value[0] }}</div>
+                                            <div>{{ $value[1] }}</div>
+                                            <div><img src="data:image/png;base64, {{ $value[2] }}" alt="" class="w-3 py-0"></div>
+                                        </div>
+
+                                    </td>
+                                @php
+                                    if (($conta % 3)==0) {
+                                @endphp
+                                </tr>
+                                @php
+                                    }
+                                @endphp
+
                         @endforeach
-                    </table>
+                    @else
+                        <div class="card-body">
+                            <strong>No existen registros</strong>
+                        </div>
+                    @endif
                 </tbody>
             </table>
-        @else
-            <div class="card-body">
-                <strong>No existen registros</strong>
-            </div>
-        @endif
+
     </x-slot>
 </x-fondo-print>
